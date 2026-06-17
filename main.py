aa3321134a653246a750d917d8f19aa7367fff4d
AEV <165625054+aevxofficial@users.noreply.github.com>
2026-06-17 09:39:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import os
 import sys
-import winreg
-
-if os.name == "nt":
-    try:
-        def calistir_arkaplan():
-            py = sys.executable
-            pythonw = os.path.join(os.path.dirname(py), "pythonw.exe")
-            start = pythonw if os.path.exists(pythonw) else py
-            script = os.path.abspath(__file__)
-            kod = f'"{start}" "{script}"'
-
-            with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run", 0, winreg.KEY_SET_VALUE) as key:
-                winreg.SetValueEx(key, "NamazVaktiPro", 0, winreg.REG_SZ, kod)
-
-        calistir_arkaplan()
-    except Exception:pass
 
 from src.app import App
 
 # Bismillahirrahmanirrahim
 # OOP,src layout customtkinter denemesi
 
+def otomatk_baslat_ayarla():
+    if os.name != "nt":return
+
+    try:
+        import winreg
+
+        py = sys.executable
+        pythonw = os.path.join(os.path.dirname(py), "pythonw.exe")
+        start = pythonw if os.path.exists(pythonw) else py
+
+        kod = f'"{start}" "{sys.executable}"'
+
+        with winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER,
+            r"Software\Microsoft\Windows\CurrentVersion\Run",
+            0,
+            winreg.KEY_SET_VALUE,
+        ) as key:
+            winreg.SetValueEx(key, "NamazVaktiPro", 0, winreg.REG_SZ, kod)
+    except Exception:
+        pass
+
+
 if __name__ == "__main__":
+    otomatk_baslat_ayarla()
     app = App()
     app.mainloop()
